d3cbe9797b7670224b6ac5ec8112592550111d56
Florian Bethe <f.bethe@gmx.de>
2018-04-03 00:28:31 +0200

    Converted project to CMake

--- CudaTest/sphere.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "vec.h"
-
-template < class T >
-struct sphere {
-	vec3<T> m_pos;
-	T rad;
-};
--- wrapper/error.hpp
+++ wrapper/error.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <exception>
+#include <string>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
